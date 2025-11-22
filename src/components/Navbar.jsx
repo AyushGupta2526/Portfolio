4740aa27e8d02c90dff422f27366df48253c3f85
Ayush Gupta <guptaayush2526@gmail.com>
2025-11-22 23:48:53 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { navLinks } from "#constants"
+
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src='/images/logo.svg' alt="logo" />
+        <p className="font-bold">Ayush's Portfolio</p>
+
+        <ul>
+          {navLinks.map(({ id, name }) => (
+            <li key={id}>
+              <p>{name} </p>
+            </li>
+          ))} 
+
+        </ul>
+
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
