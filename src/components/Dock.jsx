c27468e761780ec4c5577f1280562cd08f630f62
Ayush Gupta <guptaayush2526@gmail.com>
2025-11-24 23:20:40 +0530

    Added window store

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -3,8 +3,10 @@ import { useGSAP } from '@gsap/react';
 import { useRef } from 'react'
 import gsap from 'gsap';
 import { Tooltip } from 'react-tooltip';
+import useWindowStore from '#store/window';
 
 const Dock = () => {
+    const{ openWindow, closeWindow, windows } = useWindowStore();
     const dockRef = useRef(null);
 
     useGSAP(() => {
@@ -54,7 +56,22 @@ const Dock = () => {
 
    
 
-    const toggleApp = (app) => {};
+    const toggleApp = (app) => {
+        if(!app.canOpen) return;
+
+        const window = windows[app.id];
+
+        if(!window){
+            console.error (`Window not found for app: ${app.id}`);
+            return;
+        }
+
+        if(window.isOpen) {
+            closeWindow(app.id);
+        } else {
+            openWindow(app.id);
+        }
+    };
 
   return (
     <section id = "dock">
